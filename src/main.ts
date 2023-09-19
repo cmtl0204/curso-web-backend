4e9b47b5c8ee63b38d77fe4f7628f8edde671b7a
cmtl0204 <cesar.tamayo0204@gmail.com>
2023-09-19 17:43:17 -0500

    delete main

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-    app.setGlobalPrefix('api/v1');
-
-    const documentBuilder = new DocumentBuilder().setTitle('API IGNUG').setDescription('App description').setVersion('3').addBearerAuth().build();
-
-    const document = SwaggerModule.createDocument(app, documentBuilder);
-
-    SwaggerModule.setup('docs', app, document);
+    const app = await NestFactory.create(AppModule);
 
     await app.listen(3000);
 }
