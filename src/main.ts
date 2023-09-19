83d1b76e43f18deed7de60e4cc22b01dfa264716
cmtl0204 <cesar.tamayo0204@gmail.com>
2023-09-19 17:40:33 -0500

    add route list

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create(AppModule);
 
     app.setGlobalPrefix('api/v1');
 
@@ -15,6 +14,24 @@ async function bootstrap() {
     SwaggerModule.setup('docs', app, document);
 
     await app.listen(3000);
+
+    const server = app.getHttpServer();
+
+    const router = server._events.request._router;
+
+    const availableRoutes: [] = router.stack
+        .map(layer => {
+            if (layer.route) {
+                return {
+                    route: {
+                        path: layer.route?.path,
+                        method: layer.route?.stack[0].method,
+                    },
+                };
+            }
+        })
+        .filter(item => item !== undefined);
+    console.log(availableRoutes);
 }
 
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,46 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, Query} from '@nestjs/common';
-import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query} from '@nestjs/common';
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
     @ApiOperation({
-        summary: 'Hola Mundo',
-        description: 'Esto es una descripción',
-        externalDocs: {description: 'desc2', url: 'http://google.com'}
+        summary: 'Hello World',
+        description: 'This is a description',
+        externalDocs: {description: 'description', url: 'https://google.com'}
     })
     @Get()
     @HttpCode(HttpStatus.OK)
-    holaMundo(@Query() query: any) {
+    helloWorld(@Query() query: any) {
         return query.name;
     }
 
     @ApiOperation({
+        summary: 'Find All Users',
+        description: 'This is a description',
+        externalDocs: {description: 'url', url: 'https://google.com'}
+    })
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll() {
+        return 'users';
+    }
+
+    @ApiOperation({
+        summary: 'Find One User',
+        description: 'This is a description',
+        externalDocs: {description: 'url', url: 'https://google.com'}
+    })
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    findOne(@Param('id') id: string) {
+        return 'user';
+    }
+
+    @ApiOperation({
         summary: 'Create User',
-        description: 'Esto es una descripción',
-        externalDocs: {description: 'desc2', url: 'http://google.com'}
+        description: 'This is a description',
+        externalDocs: {description: 'description', url: 'https://google.com'}
     })
     @Post()
     @HttpCode(HttpStatus.OK)
@@ -28,12 +50,23 @@ export class UsersController {
 
     @ApiOperation({
         summary: 'Update User',
-        description: 'Esto es una descripción',
-        externalDocs: {description: 'desc2', url: 'http://google.com'}
+        description: 'This is a description',
+        externalDocs: {description: 'description', url: 'https://google.com'}
     })
     @Put(':id')
     @HttpCode(HttpStatus.OK)
     put(@Param('id') id: string, @Body() payload: any) {
         return payload;
+    }
+
+    @ApiOperation({
+        summary: 'Delete User',
+        description: 'This is a description',
+        externalDocs: {description: 'Description', url: 'https://google.com'}
+    })
+    @Delete(':id')
+    @HttpCode(HttpStatus.OK)
+    delete(@Param('id') id: string) {
+        return id;
     }
 }
